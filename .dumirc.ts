42171c090d32465e0186b4a29cfb6570613c28d9
郭小梅 <sally.guo@ersoft.cn>
2023-04-23 17:21:47 +0800

    feat: git配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'koalaui'; // 项目名
+
 export default defineConfig({
-  outputPath: 'docs-dist',
   themeConfig: {
-    name: 'koalaui',
+    name: 'koala-ui',
   },
+  resolve: {
+    codeBlockMode: 'passive',
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
